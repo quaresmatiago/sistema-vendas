10523d6acad1453ae7797cadcfd94e37ff700b5e
Tiago Quaresma <quaresmatiago@terra.com.br>
2026-02-24 19:41:36 -0300

    Primeiro commit - Sistema de Vendas

---
+++ src/main/java/br/com/quaresma/vendas/App.java
@@ -0,0 +1,17 @@
+package br.com.quaresma.vendas;
+
+/**
+ * Sistema de Vendas para o projeto do meu pai
+ * Quaresma - Estudante de DSM FATEC
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "==========================================" );
+        System.out.println( "   SISTEMA DE VENDAS - QUARESMA & PAI     " );
+        System.out.println( "==========================================" );
+        System.out.println( "Status: Ambiente Java 21 [OK]" );
+        System.out.println( "Iniciando modulo de pedidos..." );
+    }
+}
